5b97f72ba8b21e11df8ec0fb1571f0515a6ace2c
Yura <you@example.com>
2023-06-10 19:32:23 +0300

    что-то изменил

--- Lesson1/Lesson 1.1/Program.cs
+++ Lesson1/Lesson 1.1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear
+Console.WriteLine ("Введите первое значение");
+int a = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Введите второе значение");
+int b = int.Parse (Console.ReadLine ());
+if
+{
+    (a>b), max = a;
+}
+else 
+    max = b;
+Console.WriteLine (max);
