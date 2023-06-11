96c90ca8b16cb4c8b702e6deb123cfcff3f67c7e
Yura <you@example.com>
2023-06-11 11:59:33 +0300

    Первое домашнее задание

---
+++ Lesson1/Домашняя работа 1 задача/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое значение");
+int numa = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе значение");
+int numb = int.Parse(Console.ReadLine());
+int max = numa;
+if  (numb > max)
+{
+    max = numb;
+}
+else 
+{
+    max = numa;
+}
+Console.WriteLine($"Максимальное число {max}");
