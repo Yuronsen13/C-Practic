52cefe559d51cc8142114e9a9fa5e53e849e10fe
Yura <you@example.com>
2023-06-14 12:50:06 +0300

    ДЗ 2-1

--- Lesson1/ДЗ 2-1 задача/Program.cs
+++ Lesson1/ДЗ 2-1 задача/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine ());
+Console.WriteLine ($"Второй символ в числе {(number/10)%10}");
--- Lesson1/Задача 8/Program.cs
+++ Lesson1/Задача 8/Program.cs
@@ -1,8 +1,3 @@
-﻿Console.Clear();
-int num = new Random().Next(10, 100);
-int num1 = num / 10;
-int num2 = num % 10;
-int max = num1;
-if (num2 > max)
-    max = num2;
-Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+Console.Read((number/10)%10);
